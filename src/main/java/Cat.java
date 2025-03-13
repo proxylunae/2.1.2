2ec2125c6c0af3c94b66cf6ab047228fd0be87af
Kamil <1505kamil@gmail.com>
2025-03-13 14:11:56 +0500

    Реализован Класс cat, создан Prototype bean cat, реализован метод main для теста ссылок

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,22 @@
 public class Cat {
+    private String name;
+    public Cat(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void meow() {
+        System.out.printf("%s говорит \"Мяу!\"\n", name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
